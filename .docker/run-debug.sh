91aced613ba86da250981d060ea0b0876189400f
jasb <jason@becker-freelance.com>
2025-10-04 07:50:02 +0200

    Docker-Compose debugging

--- .docker/run-debug.sh
+++ .docker/run-debug.sh
@@ -1,1 +1,10 @@
-docker-compose --env-file .env.dev --env-file .env.debug up --build
+#!/bin/bash
+
+ENV_VARS=()
+for arg in "$@"; do
+  ENV_VARS+=("JAVA_TOOL_OPTIONS_${arg}=-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:5005")
+done
+
+echo "Set ENV Vars: ${ENV_VARS[@]}"
+
+env "${ENV_VARS[@]}" docker-compose --env-file .env.dev up --build
